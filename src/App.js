bdec8c7564ec463f0cdf44d902d1ec93526257eb
Ali Ahmed <ali.aviz111@gmail.com>
2021-01-25 15:27:41 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Bmr from './components/Bmr.js';
+import './components/bmr.css';
+function App() {
+  return (
+    <div className="App">
+      <Bmr/>
+     
+    </div>
+  );
+}
+
+export default App;
